df115274aa0b4192bdab458a2ce4f74cb6a76a78
Quyen M. Vo <54432044+voquyen0502@users.noreply.github.com>
2025-01-08 04:05:28 +0700

    Update balloon.js

--- js/balloon.js
+++ js/balloon.js
@@ -96,6 +96,6 @@ function randomRange(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-var picArray = ["img/1.png", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg", "img/8.jpg", "img/9.jpg", "img/10.jpg", "img/11.jpg", "img/12.jpg", "img/13.jpg"
+var picArray = ["img/1.png", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg"
 ];
 
